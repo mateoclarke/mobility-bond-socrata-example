9318cbab28e31363bd666f3583f1586d830e1482
Mateo Clarke <code@mateoclarke.com>
2017-09-15 00:40:16 -0500

    add data to state

--- src/App.js
+++ src/App.js
@@ -4,10 +4,24 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: '',
+    };
+  }
 
   getSocrataData() {
     axios.get('https://data.austintexas.gov/resource/c4e7-xwf6.json')
-      .then((res) => console.log(res));
+      .then((res) => {
+        console.log(res)
+        this.setState({
+          data: res.data,
+        })
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
   }
 
   componentWillMount() {
@@ -21,7 +35,7 @@ class App extends Component {
           <h2>Construction Projects funded by 2016 Mobility Bond</h2>
         </div>
         <p className="App-intro">
-          hi data.
+          {JSON.stringify(this.state.data)}
         </p>
       </div>
     );
